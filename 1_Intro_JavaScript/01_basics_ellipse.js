c81d5e9d496e47a9d2f2f73bc3ed8b2c49656d7f
Raymond Wang <rwroosevelt@gmail.com>
2016-04-27 18:00:39 -0500

    more materials

---
+++ 1_Intro_JavaScript/01_basics_ellipse.js
@@ -0,0 +1,13 @@
+// Example 3 ellipses for a snow man
+ellipse(200,300,150,150);
+ellipse(200,200,100,100);
+ellipse(200,150,75,75);
+
+// snow man with arms
+rect(0,370,400,30);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+
+line(160,200,60,40);
+line(240,200,340,40);
